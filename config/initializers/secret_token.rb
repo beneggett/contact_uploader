fa7b31703a89bc829072ce8edf6027b9d5c80e6c
Ben Eggett <beneggett@gmail.com>
2014-02-21 16:52:26 -0700

    Initial app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactUploader::Application.config.secret_token = 'f4acef037aac0523575fca39bbb37b916508a1cbea88cb4f8455391f6b18b406b2f0d94aa3e9e92549bc48d71421d15d2f3cbbdf84cdc0ee56905ad87c5068ed'
